3a68aad5504aaf9d2305b510a709fb44df50c18f
Nirmala Sharma <81033053+niru-creator@users.noreply.github.com>
2022-06-06 15:54:58 +0545

    Delete demo/src directory

--- demo/src/main/java/com/example/demo/dao/CategoryRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.enittiy.Category;
-
-public interface CategoryRepository 
-extends JpaRepository<Category,Integer>{
-}
