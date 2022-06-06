32eafc2c96728d17740a5b0e74950758a1716dd1
Nirmala Sharma <81033053+niru-creator@users.noreply.github.com>
2022-06-06 15:49:42 +0545

    Add files via upload

---
+++ src/main/java/com/example/demo/controller/CartController.java
@@ -0,0 +1,53 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import com.example.demo.dao.RatingRepository;
+import com.example.demo.enittiy.Rating;
+import com.example.demo.enittiy.User;
+import com.example.demo.enittiy.product;
+import com.example.demo.globalAccess.GlobalData;
+import com.example.demo.services.ProductService;
+
+@Controller
+public class CartController {
+
+	@Autowired
+	ProductService productService;
+	
+	
+	@GetMapping("/addToCart/{id}")
+	public String addToCart(@PathVariable int id) {
+		GlobalData.cart.add(productService.getProductById(id).get());
+		
+		return "redirect:/shop";
+	}
+	@GetMapping("/cart")
+	public String cartGet(Model model)
+	{
+		model.addAttribute("cartCount",GlobalData.cart.size());
+		model.addAttribute("total",GlobalData.cart.stream().mapToDouble(product::getPrice ).sum());
+		model.addAttribute("cart",GlobalData.cart);
+		return "cart";
+	}
+	@GetMapping("/cart/removeItem/{index}")
+	public String cartItemRemove(@PathVariable int index) {
+		
+		GlobalData.cart.remove(index);
+		return "redirect:/cart";
+	}
+	@GetMapping("/checkout")
+	public String checkout(Model model) {
+		model.addAttribute("total",GlobalData.cart.stream().mapToDouble(product::getPrice).sum());
+return "checkout";
+		
+		
+	}
+	
+}
